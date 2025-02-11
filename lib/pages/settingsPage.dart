d246724fe30210441bd4e07b7a82a7d7650dbe0c
prissmarquez <a01738866@tec.mx>
2025-02-11 14:32:15 -0600

    page settings

--- lib/pages/settingsPage.dart
+++ lib/pages/settingsPage.dart
@@ -1,4 +1,7 @@
+import 'package:deliver/themes/themeProvider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Settingspage extends StatelessWidget {
   const Settingspage({super.key});
@@ -8,7 +11,40 @@ class Settingspage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Settings'),
-      )
+      ),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        children: [
+          Container(
+            decoration: 
+            BoxDecoration(
+              color: Theme.of(context).colorScheme.secondary,
+              borderRadius: BorderRadius.circular(12)
+              ),
+              margin: EdgeInsets.all(15),
+              padding: EdgeInsets.all(25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                //dark mode
+                Text(
+                  "Dark Mode",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.inversePrimary
+                    ),
+                ),
+
+                //switch to change to dark mode 
+                CupertinoSwitch(
+                  value: Provider.of<Themeprovider> (context, listen: false). isDarkMode, 
+                  onChanged: (value) => Provider.of<Themeprovider> (context, listen: false). toggleTheme(),
+                  )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
