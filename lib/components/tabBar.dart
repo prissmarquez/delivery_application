37b2d02770424e6b4fc7d65085d344d29238323c
prissmarquez <a01738866@tec.mx>
2025-02-19 12:23:15 -0600

    ultimas modificaciones

--- lib/components/tabBar.dart
+++ lib/components/tabBar.dart
@@ -1,3 +1,5 @@
+import 'package:deliver/models/iceCreams.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class Tabbar extends StatelessWidget {
@@ -6,8 +8,15 @@ class Tabbar extends StatelessWidget {
   const Tabbar({
     super.key,
     required this.tabController,
-    });
+  });
 
+  List<Tab> _buildCategoriesTabs(){
+    return IceCreamsCategory.values.map((Category) {
+      return Tab(
+        text: Category.toString().split(".").last
+      );
+    }).toList();
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
